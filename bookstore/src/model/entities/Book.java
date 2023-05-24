1dd884c6ce3f4edbc65cf22b8c8e8f7e5dd19d1b
Ryann <ryann.arruda_159@hotmail.com>
2023-05-24 00:15:55 -0300

    feat: Creating the 'Book' entity
    Creating the book entity that will store all information about a book

---
+++ bookstore/src/model/entities/Book.java
@@ -0,0 +1,86 @@
+package model.entities;
+
+import java.util.List;
+
+public class Book {
+	private String name;
+	private Float price;
+	private String mainGenre;
+	private String placePublication;
+	private Integer yearPublication;
+	private List<String> authors;
+	
+	public Book(String name, Float price, String mainGenre, String placePublication, Integer yearPublication, List<String> authors) {
+		this.name = name;
+		this.price = price;
+		this.mainGenre = mainGenre;
+		this.placePublication = placePublication;
+		this.yearPublication = yearPublication;
+		this.authors = authors;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Float getPrice() {
+		return price;
+	}
+	
+	public String getMainGenre() {
+		return mainGenre;
+	}
+	
+	public String getPlacePublication() {
+		return placePublication;
+	}
+	
+	public Integer getYearPublication() {
+		return yearPublication;
+	}
+	
+	public List<String> getAuthors(){
+		return List.copyOf(authors);
+	}
+	
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((authors == null) ? 0 : authors.hashCode());
+		result = prime * result + ((mainGenre == null) ? 0 : mainGenre.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (authors == null) {
+			if (other.authors != null)
+				return false;
+		} else if (!authors.equals(other.authors))
+			return false;
+		if (mainGenre == null) {
+			if (other.mainGenre != null)
+				return false;
+		} else if (!mainGenre.equals(other.mainGenre))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
