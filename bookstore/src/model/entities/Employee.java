fb52b60eb0c326311858dbb9ccb9e27a7946f2f2
Ryann <ryann.arruda_159@hotmail.com>
2023-05-24 12:00:39 -0300

    feat: Creating the 'Employee' entity
    Creating the 'Employee' entity (superclass) that handles information regarding an employee

---
+++ bookstore/src/model/entities/Employee.java
@@ -0,0 +1,97 @@
+package model.entities;
+
+public class Employee {
+	private String name;
+	private int age;
+	private String email;
+	private Address address;
+	private String password;
+	
+	public Employee(String name, int age, String email, Address address, String password) {
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.address = address;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Address getAddress() {
+		return new Address(address);
+	}
+
+	public void setAddress(Address address) {
+		this.address = new Address(address);
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (age != other.age)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+}
