d3832415176ad115dd839ddf65166af800cea42b
Ryann <ryann.arruda_159@hotmail.com>
2023-05-25 00:37:38 -0300

    refactor: Changing from interface to class
    Changing from interface to class to ensure consistency, as interface is not used for the previous purpose

--- bookstore/src/model/entities/dao/DAOFactory.java
+++ bookstore/src/model/entities/dao/DAOFactory.java
@@ -1,17 +1,16 @@
 package model.entities.dao;
 
-import java.sql.Connection;
-
+import db.Database;
 import model.entities.dao.impl.AddressDaoJdbc;
 import model.entities.dao.impl.ClientDaoJdbc;
 
-public interface DAOFactory {
+public class DAOFactory {
 	
-	static ClientDAO getClientDAO(Connection conn) {
-		return new ClientDaoJdbc(conn);
+	public static ClientDAO getClientDAO() {
+		return new ClientDaoJdbc(Database.getConnection());
 	}
 	
-	static AddressDAO getAddressDAO(Connection conn) {
-		return new AddressDaoJdbc(conn);
+	public static AddressDAO getAddressDAO() {
+		return new AddressDaoJdbc(Database.getConnection());
 	}
 }
