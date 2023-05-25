356abda4f0c89dfea3f3a2ec89fd0f9d614f6e4c
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-05-24 23:50:42 -0300

    feat: Add SellerDao class
    This commit introduces a new class, SellerDao, which is responsible for handling database operations related to sellers. The SellerDao class provides methods for creating, retrieving, updating, deleting, listing all sellers, and listing a client from the seller records in the database.

---
+++ bookstore/src/model/entities/dao/SellerDAO.java
@@ -0,0 +1,16 @@
+package model.entities.dao;
+
+import java.util.List;
+
+import model.entities.Client;
+import model.entities.Seller;
+
+public interface SellerDAO {
+	
+	boolean insert(Seller seller);
+	Seller retrieve(String name);
+	boolean deleteById(int id);
+	boolean update(Seller seller);
+	List<Seller> listAll();
+	Client listOneClient();
+}
