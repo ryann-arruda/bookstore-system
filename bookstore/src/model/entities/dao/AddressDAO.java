1d0a2a64e418666e9f88f7cb674a6abe7c4debc7
Ryann <ryann.arruda_159@hotmail.com>
2023-05-24 23:03:39 -0300

    refactor: Changing the return type
    Changing the return type in the 'insert' method, allowing the primary key to be returned

--- bookstore/src/model/entities/dao/AddressDAO.java
+++ bookstore/src/model/entities/dao/AddressDAO.java
@@ -3,9 +3,8 @@ package model.entities.dao;
 import model.entities.Address;
 
 public interface AddressDAO {
-	boolean insert(Address address);
+	int insert(Address address);
 	Address retrive(String name);
 	boolean deleteById(int id);
 	boolean update(Address address);
-	
 }
