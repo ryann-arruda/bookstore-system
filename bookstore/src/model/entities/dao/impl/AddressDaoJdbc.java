743fbb567b2b53fa8394e27ce40179370701cc0c
Ryann <ryann.arruda_159@hotmail.com>
2023-05-24 23:13:20 -0300

    feat: Creating the AddressDaoJdbc entity
    Creating AddressDaoJdbc entity to carry out accesses to the database referring to the address entity

---
+++ bookstore/src/model/entities/dao/impl/AddressDaoJdbc.java
@@ -0,0 +1,71 @@
+package model.entities.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DatabaseException;
+import model.entities.Address;
+import model.entities.dao.AddressDAO;
+
+public class AddressDaoJdbc implements AddressDAO{
+	private Connection conn;
+	
+	public AddressDaoJdbc(Connection conn) {
+		this.conn = conn;
+	}
+
+	@Override
+	public int insert(Address address) {
+		PreparedStatement ps;
+		int rowsAffected = 0;
+		
+		try {
+			ps = conn.prepareStatement("INSERT INTO address (thoroughfare, neighborhood, complement, house_number, zip_code) " +
+										"VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			
+			ps.setString(1, address.getThoroughfare());
+			ps.setString(2, address.getNeighborhood());
+			ps.setString(3, address.getComplement());
+			ps.setInt(4, address.getNumber());
+			ps.setString(5, address.getZipCode());
+			
+			rowsAffected = ps.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+				
+				while(rs.next()) {
+					return rs.getInt(1);
+				}
+			}
+		}
+		
+		catch (SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+		return -1;
+	}
+
+	@Override
+	public Address retrive(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteById(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Address address) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
