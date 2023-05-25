f8a0196a98f738c23ac30118d8a3b9270b1e7dda
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-05-25 04:16:05 -0300

    Add files via upload

--- bookstore/src/model/entities/dao/impl/BookDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/BookDaoJdbc.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import java.util.ArrayList;
 
 import db.Database;
 import db.DatabaseException;
@@ -19,6 +21,33 @@ private Connection conn;
 		this.conn = conn;
 	}
 
+	private List<String> listAllAuthors(String title){
+		List<String> list = new ArrayList<String>();
+		
+		PreparedStatement ps = null;
+		ResultSet rs= null;
+		
+		try {
+			ps = conn.prepareStatement("SELECT author_name FROM Author A, Book_Author BA, Book B "+
+										"WHERE A.author_id=BA.author_id AND BA.book_id=B.book_id AND B.title=?");
+			
+			ps.setString(1, title);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println(rs.getString(1));
+				list.add(rs.getString(1));
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closePreparedStatement(ps);
+		}
+	}
 	@Override
 	public boolean insert(Book book) {
 		PreparedStatement ps = null;
@@ -38,6 +67,11 @@ private Connection conn;
 			rowsAffected = ps.executeUpdate();
 			ResultSet rs_book = ps.getGeneratedKeys();
 			
+			int book_id = 0;
+			rs_book.next();
+			book_id = rs_book.getInt(1);
+
+			
 			ps_author = conn.prepareStatement("INSERT INTO Author (author_name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
 			
 			int rowsAffectedAuthors = 0;
@@ -48,13 +82,10 @@ private Connection conn;
 				if(rowsAffectedAuthors > 0) {
 					ResultSet rs_author = ps_author.getGeneratedKeys();
 					int author_id = 0;
-					int book_id = 0;
 					
 					while(rs_author.next()) {
 						author_id = rs_author.getInt(1);
 						
-						rs_book.next();
-						book_id = rs_book.getInt(1);
 					}
 					
 					PreparedStatement psBook_Author = conn.prepareStatement("INSERT INTO Book_Author (book_id, author_id) VALUES " + 
@@ -81,7 +112,30 @@ private Connection conn;
 
 	@Override
 	public Book retrieve(String title) {
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			Book book = null;
+			
+			ps = conn.prepareStatement("SELECT * FROM Book WHERE title = ?");
+			ps.setString(1, title);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				book = new Book(rs.getString("title"),rs.getFloat("price"),rs.getString("main_genre"), rs.getString("place_publication"),
+								rs.getInt("year_publication"), listAllAuthors(title));
+				
+			}
+			return book;
+		}
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closePreparedStatement(ps);
+		}
 	}
 
 	@Override
