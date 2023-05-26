18d634dc13e669b726fda17e8e85a3dedf929bea
Ryann <ryann.arruda_159@hotmail.com>
2023-05-26 12:39:11 -0300

    feat: Creating the entity 'Person'
    Creating the 'Person' entity to represent anyone in our system who isn't an employee

---
+++ bookstore/src/model/entities/Person.java
@@ -0,0 +1,107 @@
+package model.entities;
+
+public class Person {
+	private String name;
+	private int age;
+	private String email;
+	private String password;
+	
+	private Address address;
+	
+	public Person(String name, int age, String email, String password, Address address) {
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Address getAddress() {
+		return new Address(address);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAddress(Address address) {
+		this.address = new Address(address);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(name);
+		sb.append(String.format("(%d years old) - ", age));
+		sb.append(String.format("[%s]\n", email));
+		sb.append("Endereço: ");
+		sb.append(address);
+		
+		return sb.toString();
+	}
+}
