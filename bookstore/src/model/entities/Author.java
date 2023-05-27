cef300c9b4887d905c5e8dd170f7669ae9acca91
Ryann <ryann.arruda_159@hotmail.com>
2023-05-26 23:00:47 -0300

    Creating some methods
    Creating a get method and a method to add a new book to the author's book list

--- bookstore/src/model/entities/Author.java
+++ bookstore/src/model/entities/Author.java
@@ -1,5 +1,6 @@
 package model.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person{
@@ -7,5 +8,14 @@ public class Author extends Person{
 	
 	public Author(String name, int age, String email, String password, Address address) {
 		super(name, age, email, password, address);
+		this.books = new ArrayList<>();
+	}
+	
+	public List<Book> getBooks(){
+		return List.copyOf(books);
+	}
+	
+	public void addBook(Book book) {
+		books.add(book);
 	}
 }
