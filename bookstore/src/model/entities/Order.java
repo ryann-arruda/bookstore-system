43585352839c9df3abb33d5f80c318773ac492a9
Ryann <ryann.arruda_159@hotmail.com>
2023-05-27 19:15:53 -0300

    feat: Linking the Order to the Payment
    Creating a method to finalize the purchase and creating the logic to return payment for the current order

--- bookstore/src/model/entities/Order.java
+++ bookstore/src/model/entities/Order.java
@@ -2,17 +2,22 @@ package model.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
+
+import model.entities.enums.PaymentType;
 
 public class Order {
 	private float totalAmount;
 	private List<Book> items;
 	
 	private Client client;
+	private Payment payment;
 	
 	public Order(Client client) {
 		this.totalAmount = 0.0f;
 		this.items = new ArrayList<Book>();
 		this.client = client;
+		payment = null;
 	}
 
 	public float getTotalAmount() {
@@ -27,6 +32,19 @@ public class Order {
 		return new Client(client);
 	}
 	
+	public Payment getPayment() {
+		
+		if(payment != null) {
+			return new Payment(payment);
+		}
+		
+		return null;
+	}
+	
+	public int quantityItems() {
+		return items.size();
+	}
+	
 	public void addNewItem(Book book) {
 		items.add(book);
 		calculateTotalAmount();
@@ -37,5 +55,11 @@ public class Order {
 		for(Book b: items) {
 			totalAmount += b.getPrice();
 		}
+	}
+	
+	public Payment finalizePurchase(PaymentType paymentType) {
+		payment = new Payment(totalAmount, paymentType, new Date(), client);
+		
+		return new Payment(payment);
 	}
 }
