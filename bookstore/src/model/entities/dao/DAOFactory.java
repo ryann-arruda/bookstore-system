69cd70dab495d90fc79120be312d3dac26dafb00
Ryann <ryann.arruda_159@hotmail.com>
2023-05-28 12:32:43 -0300

    feat: Creating the 'getBookDAO' method
    Creating the 'getBookDAO' method to retrieve the DAO corresponding to the Book entity

--- bookstore/src/model/entities/dao/DAOFactory.java
+++ bookstore/src/model/entities/dao/DAOFactory.java
@@ -2,6 +2,7 @@ package model.entities.dao;
 
 import db.Database;
 import model.entities.dao.impl.AddressDaoJdbc;
+import model.entities.dao.impl.BookDaoJdbc;
 import model.entities.dao.impl.ClientDaoJdbc;
 
 public class DAOFactory {
@@ -12,5 +13,9 @@ public class DAOFactory {
 	
 	public static AddressDAO getAddressDAO() {
 		return new AddressDaoJdbc(Database.getConnection());
+	}
+	
+	public static BookDAO getBookDAO() {
+		return new BookDaoJdbc(Database.getConnection());
 	}
 }
