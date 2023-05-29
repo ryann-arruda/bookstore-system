1a7d04caf903a56ec76a06e134c9fb0e7e632cf3
Ryann <ryann.arruda_159@hotmail.com>
2023-05-29 17:03:55 -0300

    refactor: Adding a new attribute in the Client class
    Adding a password attribute to control a client's access to a system.

--- bookstore/src/model/entities/Client.java
+++ bookstore/src/model/entities/Client.java
@@ -1,17 +1,20 @@
 package model.entities;
 
 public class Client extends Person{
+	private String password;
 	private Boolean isOnePiece;
 	private String team;
 	
 	public Client(Client client) {
-		super(client.getName(), client.getAge(), client.getEmail(), client.getPassword(), client.getAddress());
+		super(client.getName(), client.getAge(), client.getEmail(), client.getAddress());
+		this.password = client.getPassword();
 		this.isOnePiece = client.getIsOnePiece();
 		this.team = client.getTeam();
 	}
 	
 	public Client(String name, int age, String email, Boolean isOnePiece, String team, String password, Address address) {
-		super(name, age, email, password, address);
+		super(name, age, email, address);
+		this.password = password;
 		this.isOnePiece = isOnePiece;
 		this.team = team;
 	}
@@ -19,17 +22,25 @@ public class Client extends Person{
 	public Boolean getIsOnePiece() {
 		return isOnePiece;
 	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
 
 	public void setIsOnePiece(Boolean isOnePiece) {
 		this.isOnePiece = isOnePiece;
 	}
 
-	public String getTeam() {
-		return team;
-	}
-
 	public void setTeam(String team) {
 		this.team = team;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
 	public void buyBook(Order order, Book book) {
