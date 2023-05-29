25d553a7d62a4237233ca5b0760aa58478aeb745
Ryann <ryann.arruda_159@hotmail.com>
2023-05-29 17:14:13 -0300

    refactor: Removing unnecessary attributes from the Author class
    Removing password attribute

--- bookstore/src/model/entities/Author.java
+++ bookstore/src/model/entities/Author.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Author extends Person{
 	private List<Book> books;
 	
-	public Author(String name, int age, String email, String password, Address address) {
-		super(name, age, email, password, address);
+	public Author(String name, int age, String email, Address address) {
+		super(name, age, email, address);
 		this.books = new ArrayList<>();
 	}
 	
