e63986bc4e5ba85a9ab9466c5bcc7b7f2d4ceae3
Ryann <ryann.arruda_159@hotmail.com>
2023-05-29 14:58:03 -0300

    feat: Creating the insertion method
    Creating a method to insert a new author into the database

--- bookstore/src/model/entities/dao/impl/AuthorDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/AuthorDaoJdbc.java
@@ -1,9 +1,15 @@
 package model.entities.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import db.Database;
+import db.DatabaseException;
 import model.entities.Author;
+import model.entities.dao.AddressDAO;
 import model.entities.dao.AuthorDAO;
+import model.entities.dao.DAOFactory;
 
 public class AuthorDaoJdbc implements AuthorDAO {
 	
@@ -15,7 +21,34 @@ private Connection conn;
 
 	@Override
 	public boolean insert(Author author) {
-		// TODO Auto-generated method stub
+		AddressDAO addressDJ = DAOFactory.getAddressDAO();
+		PreparedStatement ps = null;
+		
+		try {
+			ps = conn.prepareStatement("INSERT INTO Author(author_name, age, email, address_id) VALUES (?,?,?,?)");
+			
+			ps.setString(1, author.getName());
+			ps.setInt(2, author.getAge());
+			ps.setString(3, author.getEmail());
+			
+			int addressId = addressDJ.insert(author.getAddress());
+			
+			ps.setInt(4, addressId);
+			
+			int rowsAffected = ps.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				return true;
+			}
+		}
+		
+		catch (SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closePreparedStatement(ps);
+		}
+		
 		return false;
 	}
 
