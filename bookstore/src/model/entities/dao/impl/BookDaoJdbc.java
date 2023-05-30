8ec6027f9235e9a854929d851f484f426b9242e3
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-05-30 09:02:35 -0300

    feat: Creating the 'retrieve' method
    Creating the retrieval method to return a book from the database given a title.

--- bookstore/src/model/entities/dao/impl/BookDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/BookDaoJdbc.java
@@ -1,10 +1,18 @@
 package model.entities.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.Database;
+import db.DatabaseException;
+import model.entities.Author;
 import model.entities.Book;
+import model.entities.dao.AuthorDAO;
 import model.entities.dao.BookDAO;
+import model.entities.dao.DAOFactory;
 
 public class BookDaoJdbc implements BookDAO{
 	private Connection conn;
@@ -21,7 +29,36 @@ public class BookDaoJdbc implements BookDAO{
 
 	@Override
 	public Book retrieve(String title) {
-		// TODO Auto-generated method stub
+		List<Author> authors = null;
+		AuthorDAO authorDao = DAOFactory.getAuthorDAO();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			Book book;
+			
+			ps =  conn.prepareStatement("SELECT * FROM Book WHERE title = ?");
+			
+			ps.setString(1, title);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				authors = authorDao.retrieveAllAuthorsBook(title);
+				book = new Book(rs.getString("title"),rs.getFloat("price"), rs.getString("main_genre"),
+								rs.getString("place_publication"), rs.getInt("year_publication"), authors);
+				
+				return book;
+			}
+		}
+		catch(SQLException e){
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closeResultSet(rs);
+			Database.closeStatement(ps);
+		}
 		return null;
 	}
 
