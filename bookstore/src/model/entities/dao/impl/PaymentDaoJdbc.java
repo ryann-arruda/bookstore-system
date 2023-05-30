3b0f6f9f4299fe643f43f9f005dbc109c6b02cfd
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-05-30 11:59:14 -0300

    feat: Add PaymentDaoJdbc class
    This commit introduces a new class, PaymentDaoJdbc, responsible for handling database operations related to payments using JDBC (Java Database Connectivity).
    
    The PaymentDaoJdbc class provides methods for creating, retrieving, updating, and deleting payment records in the database. It follows best practices for data access and incorporates the JDBC framework for efficient and secure interaction with the payment data.

---
+++ bookstore/src/model/entities/dao/impl/PaymentDaoJdbc.java
@@ -0,0 +1,32 @@
+package model.entities.dao.impl;
+
+import model.entities.Payment;
+import model.entities.dao.PaymentDAO;
+
+public class PaymentDaoJdbc implements PaymentDAO{
+
+	@Override
+	public boolean insert(Payment payment) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Payment retrieve(int idClient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteById(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Payment payment) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
