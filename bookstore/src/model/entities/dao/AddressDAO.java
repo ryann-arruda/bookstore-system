9e1dab22e227ad2ed00dc60208c43fbe11bcf2ff
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-06-05 16:34:54 -0300

    refactor: Adding new parameter
    Adding the 'zipCode' parameter

--- bookstore/src/model/entities/dao/AddressDAO.java
+++ bookstore/src/model/entities/dao/AddressDAO.java
@@ -6,6 +6,6 @@ public interface AddressDAO {
 	int insert(Address address);
 	Address retrive(int address_id);
 	boolean deleteById(int id);
-	boolean update(Address address);
+	int update(Address address, String zipCode);
 	int retrieveAddressId(String zipCode);
 }
