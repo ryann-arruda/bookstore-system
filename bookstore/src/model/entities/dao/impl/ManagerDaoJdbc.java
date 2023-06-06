fa5f0c3ab68e5aa1264a7bf6194bc7b04720b2b5
Ryann <ryann.arruda_159@hotmail.com>
2023-06-06 15:50:29 -0300

    refactor: Changing the return type
    Fixing the return type of the 'retrieve' method from 'ManagerDAO' to 'Manager'

--- bookstore/src/model/entities/dao/impl/ManagerDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/ManagerDaoJdbc.java
@@ -1,7 +1,16 @@
 package model.entities.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import db.Database;
+import db.DatabaseException;
+import model.entities.Address;
+import model.entities.Manager;
+import model.entities.dao.AddressDAO;
+import model.entities.dao.DAOFactory;
 import model.entities.dao.ManagerDAO;
 
 public class ManagerDaoJdbc implements ManagerDAO{
@@ -12,9 +21,37 @@ public class ManagerDaoJdbc implements ManagerDAO{
 	}
 
 	@Override
-	public ManagerDAO retrieve(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	public Manager retrieve(String name) {
+		AddressDAO addressDao = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Manager manager = null;
+		
+		try {
+			addressDao = DAOFactory.getAddressDAO();
+			ps = conn.prepareStatement("SELECT * FROM Manager WHERE manager_name = ?");
+			
+			ps.setString(1, name);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Address addr = addressDao.retrive(rs.getInt("address_id"));
+				
+				manager = new Manager(rs.getString("manager_name"), rs.getInt("age"), rs.getString("email"), addr, 
+									  rs.getString("manager_password"));
+			}
+			
+			return manager;
+			
+		}
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closeResultSet(rs);
+			Database.closeStatement(ps);
+		}
 	}
 
 	@Override
