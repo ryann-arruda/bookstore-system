f0791d2c4c033e574c837dbae7cc867cac14196c
Ryann <ryann.arruda_159@hotmail.com>
2023-06-06 16:21:22 -0300

    fix: Changing the order of parameters
    Wrong order of parameters, correction made

--- bookstore/src/model/entities/Manager.java
+++ bookstore/src/model/entities/Manager.java
@@ -3,6 +3,6 @@ package model.entities;
 public class Manager extends Employee {
 	
 	public Manager(String name, int age, String email, Address address, String password) {
-		super(password, age, password, address, password);	
+		super(name, age, email, address, password);	
 	}
 }
