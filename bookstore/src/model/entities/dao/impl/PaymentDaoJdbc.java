9179462d541ee785dc3b12a648c8c9a55c9a52b5
Ryann <ryann.arruda_159@hotmail.com>
2023-06-07 16:16:04 -0300

    feat: Creating the 'deleteById' method
    Excluding payment by your ID

--- bookstore/src/model/entities/dao/impl/PaymentDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/PaymentDaoJdbc.java
@@ -101,14 +101,87 @@ public class PaymentDaoJdbc implements PaymentDAO{
 
 	@Override
 	public boolean deleteById(int id) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		int rowsAffected = -1;
+		try {
+			ps = conn.prepareStatement("DELETE FROM Payment WHERE payment_id = ?");
+			
+			ps.setInt(1, id);
+			
+			rowsAffected = ps.executeUpdate();
+		}
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closeStatement(ps);
+		}
+		
+		if(rowsAffected != -1) {
+			return true;
+		}
+		
 		return false;
 	}
 
 	@Override
-	public boolean update(Payment payment) {
-		// TODO Auto-generated method stub
+	public boolean update(PaymentStatus status, int clientId) {
+		PreparedStatement ps = null;
+		int rowsAffected = -1;
+
+		try {
+			int paymentId = retrievePaymentId(clientId);
+
+			if(paymentId != -1) {
+				ps = conn.prepareStatement("UPDATE Payment SET payment_status = ? WHERE payment_id = ?");
+
+				ps.setInt(1, status.ordinal());
+				ps.setInt(2, paymentId);
+
+				rowsAffected = ps.executeUpdate();
+			}
+
+
+		}
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closeStatement(ps);
+		}
+
+		if(rowsAffected != -1) {
+			return true;
+		}
+
 		return false;
 	}
+	
+	@Override
+	public int retrievePaymentId(int clientId) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int paymentId = -1;
 
+		try {
+			ps = conn.prepareStatement("SELECT payment_id FROM Payment WHERE client_t_id = ?");
+
+			ps.setInt(1, clientId);
+
+			rs = ps.executeQuery();
+
+			while(rs.next()) {
+				paymentId = rs.getInt(1);
+			}
+		}
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		finally {
+			Database.closeResultSet(rs);
+			Database.closeStatement(ps);
+		}
+
+		return paymentId;
+	}
 }
