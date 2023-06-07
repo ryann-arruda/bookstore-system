2d30b9e4b2f5945fc2c385b930271421aff2b57e
Ryann <ryann.arruda_159@hotmail.com>
2023-06-07 15:41:56 -0300

    refactor: Changing parameters in the 'update' method

--- bookstore/src/model/entities/dao/PaymentDAO.java
+++ bookstore/src/model/entities/dao/PaymentDAO.java
@@ -1,11 +1,12 @@
 package model.entities.dao;
 
 import model.entities.Payment;
+import model.entities.enums.PaymentStatus;
 
 public interface PaymentDAO {
 	boolean insert(Payment payment);
 	Payment retrieve(int idClient);
 	boolean deleteById(int id);
-	boolean update(Payment payment);
+	boolean update(PaymentStatus status, int clientId);
 	int retrievePaymentId(int clientId);
 }
