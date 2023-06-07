47415caa3f29248ad3993b56a03561c0c573f722
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-06-07 16:00:02 -0300

    feat: creating the retrieve method
    Creating the method to recover the payment

--- bookstore/src/model/entities/dao/impl/PaymentDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/PaymentDaoJdbc.java
@@ -5,14 +5,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Date;
 
 import db.Database;
 import db.DatabaseException;
+import model.entities.Client;
 import model.entities.Payment;
 import model.entities.dao.ClientDAO;
 import model.entities.dao.DAOFactory;
 import model.entities.dao.PaymentDAO;
 import model.entities.enums.PaymentStatus;
+import model.entities.enums.PaymentType;
 
 public class PaymentDaoJdbc implements PaymentDAO{
 	private Connection conn;
@@ -61,8 +64,39 @@ public class PaymentDaoJdbc implements PaymentDAO{
 
 	@Override
 	public Payment retrieve(int idClient) {
-		// TODO Auto-generated method stub
-		return null;
+		ClientDAO clientDao = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Payment payment = null;
+		
+		try {
+			clientDao = DAOFactory.getClientDAO();
+			ps = conn.prepareStatement("SELECT * FROM Payment WHERE client_t_id = ?");
+			
+			ps.setInt(1, idClient);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Client client = clientDao.retrieve(idClient);
+				PaymentStatus paymentStatus = PaymentStatus.values()[rs.getInt("payment_status")];
+				PaymentType paymentType = PaymentType.values()[rs.getInt("payment_type")];
+				
+				payment = new Payment(rs.getFloat("total_amount"), paymentStatus, paymentType,
+									  new Date(rs.getTimestamp("payment_time").getTime()), client);
+			}
+		}
+		
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+		finally {
+			Database.closeResultSet(rs);
+			Database.closeStatement(ps);
+		}
+		
+		return payment;
 	}
 
 	@Override
@@ -72,63 +106,9 @@ public class PaymentDaoJdbc implements PaymentDAO{
 	}
 
 	@Override
-	public boolean update(PaymentStatus status, int clientId) {
-		PreparedStatement ps = null;
-		int rowsAffected = -1;
-		
-		try {
-			int paymentId = retrievePaymentId(clientId);
-			
-			if(paymentId != -1) {
-				ps = conn.prepareStatement("UPDATE Payment SET payment_status = ? WHERE payment_id = ?");
-				
-				ps.setInt(1, status.ordinal());
-				ps.setInt(2, paymentId);
-				
-				rowsAffected = ps.executeUpdate();
-			}
-			
-			
-		}
-		catch(SQLException e) {
-			throw new DatabaseException(e.getMessage());
-		}
-		finally {
-			Database.closeStatement(ps);
-		}
-		
-		if(rowsAffected != -1) {
-			return true;
-		}
-		
+	public boolean update(Payment payment) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public int retrievePaymentId(int clientId) {
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		int paymentId = -1;
-		
-		try {
-			ps = conn.prepareStatement("SELECT payment_id FROM Payment WHERE client_t_id = ?");
-			
-			ps.setInt(1, clientId);
-			
-			rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				paymentId = rs.getInt(1);
-			}
-		}
-		catch(SQLException e) {
-			throw new DatabaseException(e.getMessage());
-		}
-		finally {
-			Database.closeResultSet(rs);
-			Database.closeStatement(ps);
-		}
-		
-		return paymentId;
-	}
 }
