1fe0d9f603c12862e948b9162a015f7f677ec454
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-06-07 16:02:21 -0300

    feat: creating method overload

--- bookstore/src/model/entities/dao/ClientDAO.java
+++ bookstore/src/model/entities/dao/ClientDAO.java
@@ -6,6 +6,7 @@ public interface ClientDAO {
 	
 	boolean insert(Client client);
 	Client retrieve(String name);
+	Client retrieve(int id);
 	boolean deleteById(int id);
 	boolean update(Client client, String email);
 	int retrieveClientId(String email);
