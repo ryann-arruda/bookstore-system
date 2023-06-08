4506a9f42dd1de2312f8f9f08d3132343ee035ff
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-06-08 14:40:20 -0300

    feat: Creating the 'deleteById' method
    Excluding seller by your ID

--- bookstore/src/model/entities/dao/impl/SellerDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/SellerDaoJdbc.java
@@ -92,7 +92,29 @@ public class SellerDaoJdbc implements SellerDAO{
 
 	@Override
 	public boolean deleteById(int id) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		int rowsAffected = -1;
+		
+		try {
+			ps = conn.prepareStatement("DELETE FROM Seller WHERE seller_id = ?");
+			
+			ps.setInt(1, id);
+			
+			rowsAffected = ps.executeUpdate();
+		}
+		
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+		finally {
+			Database.closeStatement(ps);
+		}
+		
+		if(rowsAffected != -1) {
+			return true;
+		}
+		
 		return false;
 	}
 
