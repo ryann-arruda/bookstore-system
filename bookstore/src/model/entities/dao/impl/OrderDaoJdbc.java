ea1ea5585459b39dec519884410ed37f18188498
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-06-12 11:03:27 -0300

    feat: Creating 'listAllOrders' method
    Creating a method to return a list of all orders from a client

--- bookstore/src/model/entities/dao/impl/OrderDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/OrderDaoJdbc.java
@@ -1,11 +1,29 @@
 package model.entities.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import db.Database;
+import db.DatabaseException;
+import model.entities.Client;
 import model.entities.Order;
+import model.entities.dao.AddressDAO;
+import model.entities.dao.ClientDAO;
+import model.entities.dao.DAOFactory;
 import model.entities.dao.OrderDAO;
 
 public class OrderDaoJdbc implements OrderDAO{
+	
+	private Connection conn;
+	
+	public OrderDaoJdbc(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public boolean insert(Order order) {
@@ -32,9 +50,43 @@ public class OrderDaoJdbc implements OrderDAO{
 	}
 
 	@Override
-	public List<Order> listAll(int idClient) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Order> listAllOrders(int idClient) {
+		ClientDAO clientDao = DAOFactory.getClientDAO();
+		List<Order> orders = new ArrayList<>();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			ps = conn.prepareStatement("SELECT * FROM Order_t WHERE client_t_id = ?");
+			
+			ps.setInt(1, idClient);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				Client client = clientDao.retrieve(idClient); 
+				
+				Order order = new Order(client);
+				
+				orders.add(order);
+				
+			}
+			
+		}
+		
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+		finally {
+			Database.closeResultSet(rs);
+			Database.closeStatement(ps);
+		}
+		
+		
+		return orders;
 	}
 
 	@Override
