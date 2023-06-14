8edd39ca4267aef8e3ae133dabf0548acbf55e65
Ryann <ryann.arruda_159@hotmail.com>
2023-06-13 23:27:41 -0300

    refactor: Adding new parameter
    Adding a payment type to an order

--- bookstore/src/model/entities/dao/OrderDAO.java
+++ bookstore/src/model/entities/dao/OrderDAO.java
@@ -3,10 +3,11 @@ package model.entities.dao;
 import java.util.List;
 
 import model.entities.Order;
+import model.entities.enums.PaymentType;
 
 public interface OrderDAO {
 	
-	boolean insert(Order order);
+	boolean insert(Order order, PaymentType paymentType);
 	Order retrive(int idClient);
 	boolean deleteById(int id);
 	List<Order> listAllOrders(int idClient);
