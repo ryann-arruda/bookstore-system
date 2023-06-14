20ab2cfd453f30be20eb00600e061abd36f6aaaf
Ryann <ryann.arruda_159@hotmail.com>
2023-06-14 00:32:04 -0300

    feat: Creating the 'retrieveById' method
    Retrieving a payment by your ID

--- bookstore/src/model/entities/dao/PaymentDAO.java
+++ bookstore/src/model/entities/dao/PaymentDAO.java
@@ -6,6 +6,7 @@ import model.entities.enums.PaymentStatus;
 public interface PaymentDAO {
 	int insert(Payment payment);
 	Payment retrieve(int idClient);
+	Payment retrieveById(int paymentId);
 	boolean deleteById(int id);
 	boolean update(PaymentStatus status, int clientId);
 	int retrievePaymentId(int clientId);
