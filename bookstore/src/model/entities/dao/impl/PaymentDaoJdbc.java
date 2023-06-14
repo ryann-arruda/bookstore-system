57fe6fb47a08d29325fe8c5301acfd6056e99455
Ryann <ryann.arruda_159@hotmail.com>
2023-06-14 00:32:36 -0300

    feat: Creating the 'retrieveById' method
    Retrieving a payment by your ID

--- bookstore/src/model/entities/dao/impl/PaymentDaoJdbc.java
+++ bookstore/src/model/entities/dao/impl/PaymentDaoJdbc.java
@@ -191,4 +191,41 @@ public class PaymentDaoJdbc implements PaymentDAO{
 
 		return paymentId;
 	}
+
+	@Override
+	public Payment retrieveById(int paymentId) {
+		ClientDAO clientDao = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Payment payment = null;
+		
+		try {
+			clientDao = DAOFactory.getClientDAO();
+			ps = conn.prepareStatement("SELECT * FROM Payment WHERE payment_id = ?");
+			
+			ps.setInt(1, paymentId);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Client client = clientDao.retrieve(rs.getInt("client_t_id"));
+				PaymentStatus paymentStatus = PaymentStatus.values()[rs.getInt("payment_status")];
+				PaymentType paymentType = PaymentType.values()[rs.getInt("payment_type")];
+				
+				payment = new Payment(rs.getFloat("total_amount"), paymentStatus, paymentType,
+									  new Date(rs.getTimestamp("payment_time").getTime()), client);
+			}
+		}
+		
+		catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+		finally {
+			Database.closeResultSet(rs);
+			Database.closeStatement(ps);
+		}
+		
+		return payment;
+	}
 }
