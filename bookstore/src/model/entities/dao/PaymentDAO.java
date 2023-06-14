8885c9bf004b2fe1172605329f6253b45caa8ea1
Ryann <ryann.arruda_159@hotmail.com>
2023-06-14 02:30:50 -0300

    refactor:Removing unnecessary methods

--- bookstore/src/model/entities/dao/PaymentDAO.java
+++ bookstore/src/model/entities/dao/PaymentDAO.java
@@ -1,13 +1,12 @@
 package model.entities.dao;
 
+import java.util.List;
+
 import model.entities.Payment;
-import model.entities.enums.PaymentStatus;
 
 public interface PaymentDAO {
 	int insert(Payment payment);
-	Payment retrieve(int idClient);
+	public List<Payment> retrieveAllPaymentsClient(int idClient);	
+	boolean update(List<Payment> payments);
 	Payment retrieveById(int paymentId);
-	boolean deleteById(int id);
-	boolean update(PaymentStatus status, int clientId);
-	int retrievePaymentId(int clientId);
 }
